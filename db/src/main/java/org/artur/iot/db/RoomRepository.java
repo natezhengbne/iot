c5bffbd28318c1c90e4d67bdff97639af570d78b
Nate Zheng <nate.zheng@inter-systems.com>
2020-10-22 14:53:53 +1000

    rename module as its-rp-vaadin-components

--- db/src/main/java/org/artur/iot/db/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package org.artur.iot.db;
-
-import org.artur.iot.data.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, String> {
-
-}
--- roominfo/src/main/java/org/artur/iot/backend/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package org.artur.iot.backend;
-
-import org.artur.iot.data.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, String> {
-
-    public Room getByRoom(String room);
-}
